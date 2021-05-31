31bebd421e91413cd7efee6da8baefdf774a816d
Dennis Kamau <dmkamau475@gmail.com>
2021-05-31 11:27:59 +0300

    remove hashtag - # from being appended to every url

---
+++ src/components/TechStackCard.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "TechStackCard"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
+    <b-container fluid>
+      Hey
+    </b-container>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>This is the Home Page</h1>
+    <!--    <HelloWorld msg="Welcome to Vue Denmau"/>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
+
